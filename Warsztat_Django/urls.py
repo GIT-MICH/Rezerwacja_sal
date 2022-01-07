c9de3e375b8c511bc5be7d47a61fa9aec6adf509
Zlakowski <zlakos@o2.pl>
2022-01-07 17:22:03 +0100

    add_new_views

--- Warsztat_Django/urls.py
+++ Warsztat_Django/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from reservation_app.views import AddRoomView, RoomListView, DeleteRoomView
+from reservation_app.views import AddRoomView, RoomListView, DeleteRoomView, ModifyRoomView, \
+    ReservationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('room/new', AddRoomView.as_view(), name='add-room'),
+    path('room/new/', AddRoomView.as_view(), name='add-room'),
     path('', RoomListView.as_view(), name='room-list'),
     path('room/delete/<int:room_id>', DeleteRoomView.as_view(), name='delete-room'),
+    path('room/modify/<int:room_id>', ModifyRoomView.as_view(), name='modify-room'),
+    path('room/reserve/<int:room_id>', ReservationView.as_view(), name='reserve-room'),
 
 ]
--- reservation_app/views.py
+++ reservation_app/views.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.shortcuts import render, redirect
 from django.views import View
-from .models import ConferenceRoom
+from .models import ConferenceRoom, RoomReservation
 
 
 class AddRoomView(View):
@@ -54,4 +54,74 @@ class DeleteRoomView(View):
     def get(self, request, room_id):
         room = ConferenceRoom.objects.get(id=room_id)
         room.delete()
+        return redirect('room-list')
+
+
+class ModifyRoomView(View):
+    def get(self, request, room_id):
+        room = ConferenceRoom.objects.get(id=room_id)
+        return render(
+            request,
+            'reservation_app/modify_room.html',
+            {'room': room}
+        )
+
+    def post(self, request, room_id):
+        room = ConferenceRoom.objects.get(id=room_id)
+        name = request.POST.get('room-name')
+        capacity = request.POST.get('capacity')
+        capacity = int(capacity) if capacity else 0
+        projector = request.POST.get('projector') == 'on'
+
+        if not name:
+            return render(
+                request,
+                'reservation_app/modify_room.html',
+                {'room': room}, {'error': 'Nie podano nazwy sali'}
+            )
+        if capacity <= 0:
+            return render(
+                request,
+                'reservation_app/modify_room.html',
+                {'room': room}, {'error': 'Pojemność sali musi być dodatnia'}
+            )
+        if name != room.name and ConferenceRoom.objects.filter(name=name).first():
+            return render(
+                request,
+                'reservation_app/modify_room.html',
+                {'room': room}, {'error': 'Sala o podanej nazwie istnieje'}
+            )
+        room.name = name
+        room.capacity = capacity
+        room.projector_availability = projector
+        room.save()
+        return redirect('room-list')
+
+
+class ReservationView(View):
+    def get(self, request, room_id):
+        room = ConferenceRoom.objects.get(id=room_id)
+        return render(
+            request,
+            'reservation_app/reservation.html',
+            {'room': room}
+        )
+
+    def post(self, request, room_id):
+        room = ConferenceRoom.objects.get(id=room_id)
+        date = request.POST.get('reservation-date')
+        comment = request.POST.get('comment')
+        if RoomReservation.objects.filter(room=room, date=date):
+            return render(
+                request,
+                'reservation_app/reservation.html',
+                {'room': room}, {'error': 'Sala jest już zarezerwowana!'}
+            )
+        if date < str(datetime.date.today()):
+            return render(
+                request,
+                'reservation_app/reservation.html',
+                {'room': room}, {'error': 'Data jest z przeszłości!'}
+            )
+        RoomReservation.objects.create(room=room, date=date, comment=comment)
         return redirect('room-list')
