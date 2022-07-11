77b77d479c67d776fc04e2b907e1a6dc88b86139
Michał Złakowski <zlakos@o2.pl>
2022-07-11 22:58:09 +0200

    Update settings.py

--- Warsztat_Django/settings.py
+++ Warsztat_Django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9cq%o3sp&3o&i02m&1no6*9y$sc+hkm_n8484*t7uhv)zqrds!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +81,8 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'NAME': 'reservation_db',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'PASSWORD': 'coderslab',
+        'USER': 'xxx',
+        'PASSWORD': 'xxx',
     }
 }
 
